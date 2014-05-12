bff483cfb55b9b18a587ca4e0ad42fcffee22202
nuomi <newchar@gmail.com>
2014-05-13 00:28:19 +0800

    save data to split json file, skip existed file, so this scirpt could run over and again

--- download_baidu_image.py
+++ download_baidu_image.py
@@ -2,9 +2,9 @@
 
 import json, requests
 import chardet
-import os
+import os, glob
 
-api_list_album='http://image.baidu.com/channel/albums?t=album&s=2&pn=0&rn=5&acn=4&c=' #+category name
+api_list_album='http://image.baidu.com/channel/albums?t=album&s=2&pn=0&rn=512&acn=4&c=' #+category name
 api_list_album_by_user='http://image.baidu.com/albumlist/'  #+userid
 api_list_images='http://image.baidu.com/picturelist/getpicture?offset=0&size=1000&album_id=' #131427486&from=0&user_id=119622647&format=json'
 test_c='%E7%BE%8E%E5%A5%B3'
@@ -40,42 +40,62 @@ def list_images(album_id, user_id):
 def update(category):
     all_album = json.loads(list_album(category))
     albums = all_album['albums'][:int(all_album['returnNumber'])]
-    new_albums = []
     i = 1
     for album in albums:
+        filename = album['id']+'.data.json'
+
+        if os.path.exists(filename):
+            print filename, 'exist, skip.'
+            continue
+
         picture_list = list_images(album['id'], album['userId'])
+
+        if type(picture_list) is not list:
+            print album['id'], 'pictures get failed, skip'
+            continue
+
         album['picture_list'] = picture_list
-        new_albums.append(album)
-        print i
+
+        with open(filename, 'w') as f:
+            f.write(json.dumps(album))
+        print i, ', save '+filename
         i+=1
 
-    all_album['albums'] = new_albums
+def album_factory():
+    for album_file in glob.glob(os.path.join('.', '*.data.json')):
+        with open(album_file) as fd:
+            print 'processing ', album_file
+            yield json.loads(fd.read())
 
-    with open('albums.json', 'w') as f:
-        f.write(json.dumps(all_album))
-
-def export():
-    with open('albums.json') as f:
-        albums = json.loads(f.read()) 
-
-    for album in albums['albums']:
+def download():
+    my_factory = album_factory()
+    for album in my_factory:
         pwd = working_dir+album['title']
         try:
             os.mkdir(pwd.encode('utf-8'))
         except:
             pass
+
         for image in album['picture_list']:
             #print json.dumps(image)
-            filename = image['picture_name']
+            filename = pwd +'/'+image['picture_name']
             if filename[-3:] != 'jpg': 
                 filename = filename+'.jpg'
-            with open(pwd +'/'+filename, 'w') as f:
-                try:
-                    f.write(requests.get(image['download_url'], stream=True).raw.read())    
-                except:
-                    pass
+
+            if os.path.exists(filename):
+                print filename, 'exist, skip.'
+                continue
+
+            try:
+                print 'downloading', filename
+                rawimg = requests.get(image['download_url'], stream=True, timeout=3).raw.read()
+                with open(filename, 'w') as f:
+                    f.write(rawimg)    
+            except:
+                print 'download error, skip...'
+                    
 
 if __name__ == '__main__':
-    #update(test_c)
-    export()
+    update(test_c)
+    download()
 
